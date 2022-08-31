0d0441c8d0d293f27940d1492a2c75f6b57097d4
FajasH0311 <syedfarhanpn@gmail.com>
2022-08-31 21:43:23 +0530

    lists

---
+++ 17_lists.py
@@ -0,0 +1,9 @@
+## lists are similiar to strings read 5_string.py
+
+numbers = [3, 6 ,2 ,10, 8, 4]
+max = numbers[0]
+
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
