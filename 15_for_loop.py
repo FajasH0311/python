1af85ad300919693d3bfb9d7e61bcc59d27843a9
FajasH0311 <syedfarhanpn@gmail.com>
2022-08-31 21:05:53 +0530

    finished for_loop

---
+++ 15_for_loop.py
@@ -0,0 +1,7 @@
+for item in range(5, 10, 2):
+    print(item)
+
+## in this 5 is from the no begins
+## till 10th postion which is 9 because 1 is 0
+## 2 is the gap btween each no
+## to know run the program
---
+++ Assignments/15_for_loop.py
@@ -0,0 +1,8 @@
+prices = [20, 10, 30]
+
+total = 0
+for price in prices:
+    total += price
+print(f"Your Total is: {total}")
+
+## try to do this with a input command so you can type the price
